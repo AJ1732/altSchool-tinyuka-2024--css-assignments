fe31832c8748a3f22318ddd7da8b7a1f3928e7bd
AJ1732 <ejemeniboi@gmail.com>
2025-02-19 11:59:07 +0100

    chore: update navbar navigation

--- assignment-6/js/script.js
+++ assignment-6/js/script.js
@@ -1,3 +1,4 @@
+// To toggle the backgroung color of the navbar
 document.addEventListener("DOMContentLoaded", () => {
   const navbar = document.querySelector(".navbar");
 
@@ -7,5 +8,35 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       navbar.classList.remove("navbar--scrolled");
     }
+  });
+
+  // Close mobile menu when nav links are clicked
+  const mobileNavLinks = document.querySelectorAll(".nav__links--mobile a");
+  const mobileMenuCheckbox = document.getElementById("nav__mobile__toggle");
+
+  mobileNavLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileMenuCheckbox.checked = false;
+    });
+  });
+});
+
+// To remove the id "#" from the address bar
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href^="#"]').forEach((link) => {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+
+        setTimeout(() => {
+          history.replaceState(null, null, " ");
+        }, 500);
+      }
+    });
   });
 });
